b1d410be3810641e831e71af8a3ff508013a7016
emmanuel <kwizemmy2004@gmail.com>
2021-04-05 01:38:39 +0200

    [hase one done

--- src/App.svelte
+++ src/App.svelte
@@ -15,9 +15,9 @@
 
 	h1 {
 		color: #ff3e00;
-		text-transform: uppercase;
+		/* text-transform: uppercase;
 		font-size: 4em;
-		font-weight: 100;
+		font-weight: 100; */
 	}
 
 	@media (min-width: 640px) {
