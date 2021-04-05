0fb9a875984d18e20752096d6d4801fd325e6696
emmanuel <kwizemmy2004@gmail.com>
2021-04-06 00:26:25 +0200

    added buttons and event handrers

--- src/App.svelte
+++ src/App.svelte
@@ -8,6 +8,8 @@
 		<div class="card">
 			<h1>{name}!</h1>
 			<h3>{points}</h3>
+			<button class="btn" on:click={addPoint}>+1</button>
+			<button class="btn btn-dark" on:click={removePoint}>-1</button>
 		</div>
 	</div>
 	<!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
