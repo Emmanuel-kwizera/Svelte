c85bb19fe626df85e5dc942dfaa4cd0ea56e134c
emmanuel <kwizemmy2004@gmail.com>
2021-04-06 01:07:21 +0200

    loop and foreach

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,36 @@
 <script>
 	import Navbar from './Navbar.svelte';
 	import Player from './Player.svelte';
-	
+
+	let players = [
+    {
+      name: "John Doe",
+      points: 53
+    },
+    {
+      name: "Sam Smith",
+      points: 45
+    },
+    {
+      name: "Sara Wilson",
+      points: 34
+    }
+  ];
+
+
 </script>
 
 <main>
 	<Navbar />
-	
+	<div class="container">
+		{#if players.length === 0}
+			<p>No Players</p>
+			{:else}
+				{#each players as player}
+					<Player />
+				{/each}
+		{/if}
+	</div>
 </main>
 
 <style>
