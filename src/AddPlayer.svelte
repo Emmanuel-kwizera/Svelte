29f0b005baa828d241140bf900a58f2c30c12902
emmanuel <kwizemmy2004@gmail.com>
2021-04-08 00:36:35 +0200

    adding function

--- src/AddPlayer.svelte
+++ src/AddPlayer.svelte
@@ -1,13 +1,13 @@
 <script>
   import { createEventDispatcher } from "svelte";
-  
+
     let player = {
       name: "",
       points: 0
     };
 </script>
 
-<form class="grid-3">
+<form on:submit={onSubmit} class="grid-3">
   <input type="text" placeholder="Player Name" 
   bind:value={player.name}>
   <input type="number" placeholder="Player Points" 
