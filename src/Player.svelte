d019ffc0ae49d50318d786314f48ffe6549e7745
emmanuel <kwizemmy2004@gmail.com>
2021-04-12 00:19:11 +0200

    adding new input

--- src/Player.svelte
+++ src/Player.svelte
@@ -27,6 +27,7 @@
 				<button class="btn" on:click={addPoint}>+1</button>
 				<button class="btn btn-dark" on:click={removePoint}>-1</button>
 				<input type="number" bind:value={points}>
+				<input type="text">
 			{/if}
 		</div>
 	</div>
